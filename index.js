2357b5b2f4b5d6ab947f0d7aa19d297101355e72
“techsavyy <prakhar.aggarwal.ug20@nsut.ac.in>
2023-09-02 11:39:38 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
+const server = http.createServer(app);
+const socketIO = require('socket.io');
+const io = new socketIO.Server(server);
+
+io.on('connection', (socket)=>{
+    
+
+    socket.on('username', ({username})=>{
+
+        socket.on('send-msg', (data)=>{
+            io.emit('received-msg', {
+                msg:data.msg,
+                id:socket.id,
+                username
+            });
+        })
+
+    })
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 4444;
+server.listen(PORT, ()=>{
+    console.log('Server is up at PORT', PORT);
+})
